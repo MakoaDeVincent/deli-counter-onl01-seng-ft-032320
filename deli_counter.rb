8497d58222ab1aede44b22c07503620cde3cb5b6
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-25 22:58:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.length > 0
     serving = "The line is currently: "
     katz_deli.each_with_index do |name, index|
-      serving += "#{index += 1}. #{name}"
+      serving += "#{index += 1}. #{name} "
     end
     puts serving
   else
