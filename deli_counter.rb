68160c59160af1336dab28b4eed1282321fde8af
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-25 22:26:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line(name)
     arr.each_with_index do |name, index|
       arr << "#{index += 1}. #{name}"
     return "The line is currently: #{arr}"
+  end
   else
     puts "The line is currently empty."
   end
