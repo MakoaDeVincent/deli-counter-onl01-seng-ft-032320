b4395110d1fb70f7f0564f7f552ccdfa3f2e76bc
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-25 22:14:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,12 @@
 # Write your code here.
 def line(name)
   arr = []
-  if arr.length == 0
+  if arr.length > 0
+    arr.each_with_index do |name, index|
+      return "#{index += 1}. #{name}" 
+  else
     puts "The line is currently empty."
   end
 end
+
+
